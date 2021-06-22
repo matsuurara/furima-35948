24bbd358b7471399b59e035af8ca4ceef510add6
matsuurara <tact1997asdf@gmail.com>
2021-06-22 15:32:46 +0900

    バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true,
+            format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/ }
+  validates :first_name_kana, presence: true,
+            format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/ }
   validates :birthday,presence: true
 end
