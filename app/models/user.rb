4204e779f73bc7f8cdd2ae0558d2249947375ce5
matsuurara <tact1997asdf@gmail.com>
2021-06-22 14:43:33 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presense: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presense: true
+  validates :birthday,presence: true
 end
