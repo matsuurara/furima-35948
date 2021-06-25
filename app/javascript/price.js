98423d51ae537e65f95cd7d952c2eb6dff333e56
matsuurara <tact1997asdf@gmail.com>
2021-06-25 16:33:00 +0900

    販売手数料、販売利益の表示のためのJavaScriptを作成

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,10 +1,14 @@
 function price() {
   const itemPrice = document.getElementById("item-price")
-  itemPrice.addEventListener("")
-  const addTaxPrice = document.getElementById("add-tax-price")
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+  itemPrice.addEventListener('input',() =>{
+    const inputValue = document.getElementById("item-price").value;
+        let tax = Math.floor(inputValue * 0.1).toLocaleString();
+        addTaxDom.innerHTML = tax ;
+        profitDom.innerHTML = Math.ceil(inputValue * 0.9).toLocaleString();
+  })
 
 }
-
-
 
 window.addEventListener('load', price);
