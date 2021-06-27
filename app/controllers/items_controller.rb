577e2b40a1b6c1f34290be0e5c48046977b0b2ee
matsuurara <tact1997asdf@gmail.com>
2021-06-27 18:30:48 +0900

    ルーティングとアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -22,6 +22,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+
+  end
   private
 
   def items_params
