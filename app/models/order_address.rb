efc7bf93a06622687dfb93585176b2c3e1366a63
matsuurara <tact1997asdf@gmail.com>
2021-07-01 11:48:39 +0900

    各idのバリデーションを追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,6 +10,8 @@ class OrderAddress
     validates :house_number
     validates :telephone_number, format: { with: /\A[0-9]+\z/ }, length: { minimum: 10, maximum: 11 }
     validates :token
+    validates :user_id
+    validates :item_id
   end
 
   def save
